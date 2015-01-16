7f261f69c793d235c33971676b108b5d79aa0068
Iván Sánchez Ortega <ivan@mazemap.no>
2015-01-16 10:18:31 +0100

    Fixes shadow positioning, added alignment option.

--- README.md
+++ README.md
@@ -4,8 +4,6 @@ A Leaflet icon which wraps around another L.Icon and provides a HTML label next 
 
 # Usage
 
-
-
     // Create an instance of L.Icon (or a subclass)
     var originalIcon = new L.Icon();
     
@@ -16,4 +14,15 @@ A Leaflet icon which wraps around another L.Icon and provides a HTML label next 
     var marker = L.marker(coordinages,{icon:icon});
 
 
+# Options
 
+
+    var labeledIcon = new L.LabeledIcon({
+        html: '<strong>Foobar!</strong>',   // Label contents
+        className: 'leaflet-labeled-icon',  // Optional CSS class name
+        margin: 5,     // Margin from the image ot the label, in pixels
+        icon: false,   // An instance of L.Icon
+        align: 'right' // Position of the label relative to the icon, 'left' or 'right'.    
+    })
+
+The only required options are `icon` and `html`, the rest are optional.
--- leaflet.labeled-icon.js
+++ leaflet.labeled-icon.js
@@ -2,12 +2,13 @@
  * L.LabeledIcon is a L.Icon which wraps over another L.Icon, and adds a label to it.
  */
 
-L.LabeledIcon = L.DivIcon.extend({
+L.LabeledIcon = L.Icon.extend({
     options: {
         html: '',
         className: 'leaflet-labeled-icon',
-        margin: 2,   // Pixels from the icon image
-        icon: false // An instance of L.Icon
+        margin: 5,   // Pixels from the icon image
+        icon: false, // An instance of L.Icon
+        align: 'right' // Position of the label relative to the icon, 'left' or 'right'.
     },
 
     createIcon: function (oldIcon) {
@@ -20,26 +21,33 @@ L.LabeledIcon = L.DivIcon.extend({
         div.style.position = 'relative';
         label.style.position = 'absolute';
         label.style.display  = 'block';
-        
+        label.className      = options.align;
+
         // Label position will be relative to the icon's anchor pixel.
-        label.style.left = options.icon.options.iconSize[0] / 2 + options.margin + 'px';
         /// FIXME: think whether the vertical alignment should line up with the icon,
         ///   with the anchor, or with something else.
-        label.style.top  = - options.icon.options.iconSize[1] + 'px';
+        /// Using some CSS like top: calc(-[offset]px - 50%) should work in theory.
+        var iconSize   = options.icon.options.iconSize;
+        var iconAnchor = options.icon.options.iconAnchor;
+        if (options.align === 'left') {
+            label.style.right = (iconAnchor[0] + options.margin) + 'px';
+            label.style.top   = (-iconAnchor[1]) + 'px'; // Topline of icon
+        } else {    // 'right'
+            label.style.left = (iconSize[0] - iconAnchor[0] + options.margin) + 'px';
+            label.style.top   = (-iconAnchor[1]) + 'px'; // Topline of icon
+        }
 
-        div.appendChild(label);
         if (options.icon) {
             div.appendChild(options.icon.createIcon());
         }
         
+        div.appendChild(label);
         this._setIconStyles(div, 'icon');
 
         return div;
     },
     
     createShadow: function (oldIcon) {
-        /// FIXME: Doesn't correctly apply the shadow anchoring coordinates
-        /// Probably a scope problem.
         return this.options.icon.createShadow(oldIcon);
     }
 });
